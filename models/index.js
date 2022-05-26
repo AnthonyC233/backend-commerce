27ad931da7c998393e8a9a81d4365d3aa350a336
AnthonyC233 <changanthony61@yahoo.com>
2022-05-26 14:31:10 -0400

    added code into index.js. importing and exporting my models

---
+++ models/index.js
@@ -0,0 +1,12 @@
+// Import the models
+const Product = require('./Product');
+const Category = require('./Catergory');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+  };
