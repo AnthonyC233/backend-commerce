4b9be45029ffafd5febcdad1d9dd3127e5977481
AnthonyC233 <changanthony61@yahoo.com>
2022-05-28 16:42:05 -0400

    added code in index and product js

--- models/index.js
+++ models/index.js
@@ -4,6 +4,30 @@ const Category = require('./Catergory');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Act 21 product belongs to Category.
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+// Category has many products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Act 23, These products belong to many Tags
+Product.belongsToMany(Tag, {
+  throuh: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// These tags belong to many products
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+
 module.exports = {
     Product,
     Category,
