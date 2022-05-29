b6c5023527799be9bf81b75ed78d41b10e1743d8
AnthonyC233 <changanthony61@yahoo.com>
2022-05-29 00:03:09 -0400

    update and fixed code

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,33 +1,53 @@
 const router = require('express').Router();
-const res = require('express/lib/response');
 const { Tag, Product, ProductTag } = require('../../models');
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     Tag.findAll({
-        include: [
-            {
-            model: Product,
-            through: ProductTag,
-            }
-        ]
+      include: [Product],
     })
-});
+      .then((categories) => res.json(categories))
+      .catch((err) => res.status(500).json(err));
+  });
 
 // gets id by single tag
-router.get('/:id', (req,res) => {
-
-});
+router.get('/:id', (req, res) => {
+    Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [Product],
+    })
+      .then((category) => res.json(category))
+      .catch((err) => res.status(400).json(err));
+  });
 
 // Creates tag
 router.post('/', (req, res) => {
-});
+    Tag.create(req.body)
+      .then((category) => res.status(200).json(category))
+      .catch((err) => res.status(400).json(err));
+  });
 
 // Update tag's name by value
 router.put('/:id', (req, res) => {
+    Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((category) => res.status(200).json(category))
+      .catch((err) => res.status(400).json(err));
   });
 
 // Delete tag by id value
 router.delete('/:id', (req, res) => {
-});
+    Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((category) => res.status(200).json(category))
+      .catch((err) => res.status(400).json(err));
+  });
 
 module.exports = router;
